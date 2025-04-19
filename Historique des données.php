29d3b0e56b9f0ff9bef4f4948d0d569c335362ed
Bouchra98-creat <laredjbouchra98@gmail.com>
2025-04-19 09:45:42 +0100

    Update Historique des données.php

--- Historique des données.php
+++ Historique des données.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Historique des données</title>
-    <link rel="stylesheet" href="style.css1?v=2">
-</head>
-<body>
-   
-    <p>je dois avoir la base de donnée pour l'historique </p>
-</body>
-</html>
+
